1d4a94d2864c3f3f938f7eec3b19680ea65a0d85
wyx <wyx@yizhisec.com>
2021-11-04 10:02:45 +0800

    helm chart 中间件高可用

---
+++ kafka_ha/image/a.sh
@@ -0,0 +1,8 @@
+docker save ranchercharts/confluentinc-cp-ksql-server:5.3.0 > 7d416958c4ae.tar
+docker save ranchercharts/confluentinc-cp-kafka-connect:5.3.0 > 4418f27b597c.tar
+docker save ranchercharts/confluentinc-cp-schema-registry:5.3.0 > c9d6dbcbe81e.tar
+docker save ranchercharts/confluentinc-cp-kafka-rest:5.3.0 > 72ae98e6b243.tar
+docker save ranchercharts/confluentinc-cp-kafka:5.3.0 > f9936a4fcbce.tar
+docker save ranchercharts/confluentinc-cp-zookeeper:5.3.0 > 9a664e35e1a8.tar
+docker save ranchercharts/landoop-kafka-topics-ui:latest > cecb5defe47e.tar
+docker save solsson/kafka-prometheus-jmx-exporter:latest > 36f9e9d8127b.tar
